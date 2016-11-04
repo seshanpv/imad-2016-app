696c35ec5932b57c07d1a72757fcbb0e26fce53c
seshanpv <seshanpv@gmail.com>
2016-11-04 20:32:21 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,6 +13,38 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
+app.get('/ui/assests/animate.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'animate.css'));
+});
+
+
+app.get('/ui/assets/jquery.fittext.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'jquery.fittext.js'));
+});
+
+app.get('/ui/assets/jquery.lettering.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'jquery.lettering.js'));
+});
+
+app.get('/ui/assets/jquery.textillate.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'jquery.textillate.js'));
+});
+
+app.get('/ui/assets/style.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+});
+
+app.get('/ui/css/bootstrap.min.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'bootstrap.min.css'));
+});
+app.get('/ui/css/scrolling-nav.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'scrolling-nav.css'));
+});
+app.get('/ui/assets/style.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+});
+
+
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
