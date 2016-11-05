1e95cfd56f07fdd439f096e04ac294d5162c32b0
seshanpv <seshanpv@gmail.com>
2016-11-05 18:13:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -29,6 +29,10 @@ app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
+app.get('/ui/style.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+});
+
 app.get('/images/colors2.gif', function (req, res) {
   res.sendFile(path.join(__dirname, 'images', 'colors2.gif'));
 });
